f01b006daf7d5f67a218ce37451a774989e60544
Daniel Laanpere <daniel.laanpere@gmail.com>
2020-02-16 16:48:35 +0200

    setup android build to succeed

--- android/app/src/main/java/com/obby/MainApplication.java
+++ android/app/src/main/java/com/obby/MainApplication.java
@@ -33,6 +33,8 @@ public class MainApplication extends Application implements ReactApplication {
         protected List<ReactPackage> getPackages() {
           @SuppressWarnings("UnnecessaryLocalVariable")
           List<ReactPackage> packages = new PackageList(this).getPackages();
+          // packages.add(new MainReactPackage());
+          packages.add(new ModuleRegistryAdapter(mModuleRegistryProvider));
           // Packages that cannot be autolinked yet can be added manually here, for example:
           // packages.add(new MyReactNativePackage());
           return packages;
