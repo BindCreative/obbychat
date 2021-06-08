3df855adc412c84822de24c277266a16e1b82dc8
nikolay.natorin <nikolaynatorin@ratiose.com>
2021-06-08 10:25:08 +0300

    #41

--- App.js
+++ App.js
@@ -1,10 +1,12 @@
 console.disableYellowBox = true;
 import './shim.js';
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Provider } from 'react-redux';
 import { PersistGate } from 'redux-persist/integration/react';
 import { Provider as PaperProvider } from 'react-native-paper';
 import { RootSiblingParent } from 'react-native-root-siblings';
+
+import NotificationService from './NotificationService';
 
 import Container from './src/screens/Container';
 
@@ -12,16 +14,31 @@ import configureStore from './src/store/configureStore';
 
 export const storeSetup = configureStore();
 
-const App = () => (
-  <RootSiblingParent>
-    <Provider store={storeSetup.store}>
-      <PersistGate persistor={storeSetup.persistor}>
-        <PaperProvider>
-          <Container />
-        </PaperProvider>
-      </PersistGate>
-    </Provider>
-  </RootSiblingParent>
-);
+const App = () => {
+  const [deviceToken, setDeviceToken] = useState("");
+
+  const handleRegister = (token) => {
+    setDeviceToken(token);
+    console.log(token);
+  };
+
+  const handleNotification = (notification) => {
+    console.log(notification);
+  };
+
+  const notificationService = new NotificationService(handleRegister, handleNotification);
+
+  return (
+    <RootSiblingParent>
+      <Provider store={storeSetup.store}>
+        <PersistGate persistor={storeSetup.persistor}>
+          <PaperProvider>
+            <Container />
+          </PaperProvider>
+        </PersistGate>
+      </Provider>
+    </RootSiblingParent>
+  );
+};
 
 export default App;
---
+++ NotificationHandler.js
@@ -0,0 +1,60 @@
+import { Platform } from 'react-native'
+import PushNotification from 'react-native-push-notification';
+
+class NotificationHandler {
+  token = "";
+  onNotification(notification) {
+    console.log('NotificationHandler:', notification);
+
+    if (typeof this._onNotification === 'function') {
+      this._onNotification(notification);
+    }
+  }
+
+  onRegister(token) {
+    this.token = token;
+    if (typeof this._onRegister === 'function') {
+      this._onRegister(token);
+    }
+  }
+
+  onAction(notification) {
+    console.log ('Notification action received:');
+    console.log(notification.action);
+    console.log(notification);
+
+    if(notification.action === 'Yes') {
+      PushNotification.invokeApp(notification);
+    }
+  }
+
+  onRegistrationError(err) {
+    console.log(err);
+  }
+
+  attachRegister(handler) {
+    this._onRegister = handler;
+  }
+
+  attachNotification(handler) {
+    this._onNotification = handler;
+  }
+}
+
+const handler = new NotificationHandler();
+
+PushNotification.configure({
+  onRegister: handler.onRegister.bind(handler),
+  onNotification: handler.onNotification.bind(handler),
+  onAction: handler.onAction.bind(handler),
+  onRegistrationError: handler.onRegistrationError.bind(handler),
+  permissions: {
+    alert: true,
+    badge: true,
+    sound: true,
+  },
+  popInitialNotification: true,
+  requestPermissions: Platform.OS === 'ios'
+});
+
+export default handler;
